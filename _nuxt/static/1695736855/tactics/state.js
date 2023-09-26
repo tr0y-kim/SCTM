caf33d04f73e532cad8f39d2cb95bcb60838c4a5
tr0y-kim <tr0y-kim@users.noreply.github.com>
2023-09-26 14:00:59 +0000

    deploy: 9a3f78479d79872028f3fc1aa7d27fd9b4425ddc

---
+++ _nuxt/static/1695736855/tactics/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FSCTM\u002F_nuxt\u002Fstatic\u002F1695736855",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftactics",config:{_app:{basePath:"\u002FSCTM\u002F",assetsPath:"\u002FSCTM\u002F_nuxt\u002F",cdnURL:a}}}}(null));
