6cd2f2d309890cecac121200b51cd425a4fab40d
tr0y-kim <tr0y-kim@users.noreply.github.com>
2023-11-24 12:09:10 +0000

    deploy: cf62b3a332c4e5888101de4f9de20b1644fc9250

--- _nuxt/static/1700826907/tactics/TA0001/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FSCTM\u002F_nuxt\u002Fstatic\u002F1700826907",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftactics\u002FTA0001",config:{_app:{basePath:"\u002FSCTM\u002F",assetsPath:"\u002FSCTM\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1700827745/tactics/TA0008/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FSCTM\u002F_nuxt\u002Fstatic\u002F1700827745",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftactics\u002FTA0008",config:{_app:{basePath:"\u002FSCTM\u002F",assetsPath:"\u002FSCTM\u002F_nuxt\u002F",cdnURL:a}}}}(null));
